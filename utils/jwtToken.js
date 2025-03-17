b9ecb1f917790205d74f0fdd73374a6cab17bde3
suraj <tripurarikumarsharma123@gmail.com>
2025-03-17 08:27:54 +0530

    some new changes

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,7 +4,9 @@ export const sendToken = (user, statusCode, res, message) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true, // Set httpOnly to true
+    httpOnly: true,
+    secure:true,
+    sameSite:"None" // Set httpOnly to true
   };
 
   res.status(statusCode).cookie("token", token, options).json({
